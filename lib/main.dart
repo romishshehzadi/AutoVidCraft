71dfbd3744a1c555b811f00ba40e8d177d47f5f5
romishshehzadi <romishshehzadi0@gmail.com>
2026-09-12 18:06:57 +0500

    Secure API keys setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ import 'Screens/Dashboard.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
